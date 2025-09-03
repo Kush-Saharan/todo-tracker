18a5e5e05948fcd4ee272bf66f505652ded85844
Kush-Saharan <kush.saharan29@gmail.com>
2025-09-03 18:19:36 +0530

    Made basic user flow

--- todo-tracker-flask/app/routes/tasks_routes.py
+++ todo-tracker-flask/app/routes/tasks_routes.py
@@ -5,16 +5,28 @@ tasks_bp = Blueprint('tasks', __name__)
 
 @tasks_bp.route('/')
 def home():
-    # tasks=Tasks.query.all()
-    return render_template("index.html")
+    tasks=Tasks.query.all()
+    return render_template("index.html",tasks=tasks)
 
 
-@tasks_bp.route('/add_task')
+@tasks_bp.route('/task/add',methods=['POST'])
 def add_task():
-    pass
 
+    task_add=request.form['task']
+    existing_task = Tasks.query.filter(Tasks.task_name.ilike(task_add)).first()
+    if(existing_task):
+        message="This task already exists in tasks!"
+    else:
+        new_task=Tasks(task_name=task_add)
+        db.session.add(new_task)
+        db.session.commit()
+        message="Task added successfully!"
 
-@tasks_bp.route('/update_task')
+    tasks=Tasks.query.all()
+    return render_template('index.html',tasks=tasks, message=message)
+    
+
+@tasks_bp.route('/task/update')
 def update_task():
     pass
 
--- todo-tracker-flask/run.py
+++ todo-tracker-flask/run.py
@@ -1,6 +1,8 @@
-from app.main import create_app
+from app.main import create_app,db
 
 app=create_app()
 
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
